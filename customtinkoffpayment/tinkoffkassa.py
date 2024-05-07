c5e0ac82fe8b34f77c4765b7ca0caa72ea5ec101
Aleksandr Zonov <2281337a@gmail.com>
2024-05-07 08:50:20 +0800

    Add cancel method to TInkoffKassa class

--- customtinkoffpayment/tinkoffkassa.py
+++ customtinkoffpayment/tinkoffkassa.py
@@ -54,7 +54,7 @@ class TinkoffKassa:
 
         return response
 
-    def get_state(self, payment_id: str) -> ...:
+    def get_state(self, payment_id: str) -> t.Dict[t.Union[str, int]]:
         "Метод возвращает статус платежа."
 
         url: str = "https://securepay.tinkoff.ru/v2/GetState"
@@ -62,6 +62,33 @@ class TinkoffKassa:
         request_body: dict[t.Union[str, int]] = {
             "TerminalKey": self.terminalkey,
             # "PaymentId": pt.get("externalid"),
+            "PaymentId": payment_id
+        }
+        request_body.update({"Token": self.generate_token(
+            request_body=request_body, password=self.terminalpsw)})
+
+        response: dict[t.Union[str, int]] = requests\
+            .post(url=url, json=request_body)\
+            .json()
+
+        return response
+
+    def cancel(self, payment_id: str) -> t.Dict[t.Union[str, int]]:
+        """
+        Отменяет платежную сессию. В зависимости от статуса платежа переводит его в следующие состояния:\n
+        NEW - CANCELED\n
+        AUTHORIZED - PARTIAL_REVERSED – если отмена не на полную сумму\n
+        AUTHORIZED - REVERSED - если отмена на полную сумму\n
+        CONFIRMED - PARTIAL_REFUNDED – если отмена не на полную сумму\n
+        CONFIRMED - REFUNDED – если отмена на полную сумму.\n
+        Если платеж находился в статусе AUTHORIZED производится отмена холдирования средств на карте клиента. \n
+        При переходе из статуса CONFIRMED – возврат денежных средств на карту клиента.\n
+        """
+
+        url: str = "https://securepay.tinkoff.ru/v2/Cancel"
+
+        request_body: dict[t.Union[str, int]] = {
+            "TerminalKey": self.terminalkey,
             "PaymentId": payment_id
         }
         request_body.update({"Token": self.generate_token(
